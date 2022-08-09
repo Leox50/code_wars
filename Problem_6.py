0c527d7d55886ff3304dbfc01fc5ebf80cfb7531
Leox50 <vk20xx@gmail.com>
2022-08-09 18:33:19 +0300

    Add file

---
+++ Problem_6.py
@@ -0,0 +1,22 @@
+'''
+
+Take 2 strings s1 and s2 including only letters from a to z.
+Return a new sorted string, the longest possible,
+containing distinct letters -
+each taken only once - coming from s1 or s2.
+
+Example:
+
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+'''
+
+def longest(a1, a2):
+    # I am using the set function as it returns only unique numbers
+    # however, set func also has an unpredictable behavior, so we need to
+    # get back to list and sort it.
+    return ''.join([str(i) for i in sorted(list(set(a1+a2)))])
+
+longest("aretheyhere", "yestheyarehere")
